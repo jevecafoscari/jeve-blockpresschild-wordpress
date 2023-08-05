44425560dc9ff859850b37cbf08e6c8aef7340d5
Emilio Dalla Torre <info@emiliodallatorre.it>
2023-08-05 13:08:03 +0200

    Inserito il filtro per la query.

--- functions.php
+++ functions.php
@@ -11,3 +11,42 @@ function childtheme_parent_styles()
 }
 
 add_action("wp_enqueue_scripts", "childtheme_parent_styles");
+
+
+/**
+ * Update the posts widget or portfolio widget query.
+ *
+ * @param \WP_Query $query The WordPress query instance.
+ * @since 1.0.0
+ */
+function blog_query_callback($query)
+{
+    if ($_GET['selected_category']) {
+        $tax_query = array(
+            array(
+                'taxonomy' => 'category',
+                'field' => 'slug',
+                'terms' => $_GET['selected_category'],
+            )
+        );
+
+        $query->set('tax_query', $tax_query);
+    }
+}
+
+add_action('elementor/query/blog_query_id', 'blog_query_callback');
+
+if (!function_exists('write_log')) {
+
+    function write_log($log)
+    {
+        if (true === WP_DEBUG) {
+            if (is_array($log) || is_object($log)) {
+                error_log(print_r($log, true));
+            } else {
+                error_log($log);
+            }
+        }
+    }
+
+}
